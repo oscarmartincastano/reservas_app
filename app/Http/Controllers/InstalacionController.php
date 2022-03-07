b7c03e53d7f4fceac68d2f8039d684257a3f371a
Arizaser <36188739+Arizaser@users.noreply.github.com>
2022-03-07 13:03:15 +0100

    emails

--- app/Http/Controllers/InstalacionController.php
+++ app/Http/Controllers/InstalacionController.php
@@ -177,8 +177,8 @@ class InstalacionController extends Controller
             }
         }
 
-        Mail::to($reserva->user->email)->send(new ReservaAdmin($reserva->user, $reserva));
-        Mail::to(auth()->user()->instalacion->user_admin->email)->send(new NewReserva(auth()->user(), $reserva));
+        /* Mail::to($reserva->user->email)->send(new ReservaAdmin($reserva->user, $reserva));
+        Mail::to(auth()->user()->instalacion->user_admin->email)->send(new NewReserva(auth()->user(), $reserva)); */
 
         return redirect($request->slug_instalacion . '/admin');
     }
@@ -231,7 +231,7 @@ class InstalacionController extends Controller
 
     public function listado_todas_reservas(Request $request)
     {
-        $reservas = Reserva::where('reserva_periodica', '!=', 0)->whereIn('id_pista', Pista::where('id_instalacion', auth()->user()->instalacion->id)->pluck('id'))->get();
+        $reservas = Reserva::where('reserva_periodica', null)->whereIn('id_pista', Pista::where('id_instalacion', auth()->user()->instalacion->id)->pluck('id'))->get();
 
         return view('instalacion.reservas.list', compact('reservas'));
     }
