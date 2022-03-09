80caf8db87c8bf8527e8df4244f7972afabb442c
Arizaser <36188739+Arizaser@users.noreply.github.com>
2022-03-09 11:19:59 +0100

    input week value

--- resources/views/instalacion/home.blade.php
+++ resources/views/instalacion/home.blade.php
@@ -337,7 +337,7 @@
                                         <button class="btn-week">{!! (!isset(request()->semana) && !isset(request()->week)) || (request()->semana == 0 && !isset(request()->week)) ? 'Semana actual' :  \Carbon\Carbon::parse(iterator_to_array($period)[0])->formatLocalized('%d %b') . ' - ' . \Carbon\Carbon::parse(iterator_to_array($period)[count(iterator_to_array($period))-1])->formatLocalized('%d %b') !!}</button>
                                         <div class="btn-wrp">
                                             <form action="#" method="get">
-                                                <input type="week" name="week" id="week" value="{{ isset(request()->week) ? request()->week : '' }}" class="btn-clck">
+                                                <input type="week" name="week" id="week" value="{{ \Carbon\Carbon::parse(iterator_to_array($period)[0])->format('Y').'-W'.\Carbon\Carbon::parse(iterator_to_array($period)[0])->format('W') }}" class="btn-clck">
                                             </form>
                                         </div>
                                     </div>
