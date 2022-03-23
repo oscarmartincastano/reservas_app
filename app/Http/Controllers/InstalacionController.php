9c9eb8e807537cd9b1a1b28949a526d4777ebd41
Arizaser <36188739+Arizaser@users.noreply.github.com>
2022-03-23 12:57:20 +0100

    otro arreglo

--- app/Http/Controllers/InstalacionController.php
+++ app/Http/Controllers/InstalacionController.php
@@ -311,9 +311,9 @@ class InstalacionController extends Controller
         return redirect('/'.request()->slug_instalacion.'/admin/reservas/periodicas');
     }
 
-    /* public function arreglos_reservas()
+    public function arreglos_reservas()
     {
-        $reservas = Reserva::where([['timestamp', '>=', strtotime('2022-03-27 00:00')], ['timestamp', '<=', strtotime('2022-10-30 00:00')]], ['reserva_periodica', '!=', null])->get();
+        $reservas = Reserva::where([['timestamp', '>=', strtotime('2022-03-27 00:00')], ['timestamp', '<=', strtotime('2022-10-30 00:00')]], ['creado_por', 'admin'])->get();
 
         foreach ($reservas as $reserva) {
             $res = Reserva::find($reserva->id);
@@ -329,7 +329,7 @@ class InstalacionController extends Controller
         }
 
         return Reserva::where([['timestamp', '>=', strtotime('2022-03-27 00:00')], ['timestamp', '<=', strtotime('2022-10-30 00:00')]])->get();
-    } */
+    }
 
     public function borrar_reservas_periodicas(Request $request)
     {
