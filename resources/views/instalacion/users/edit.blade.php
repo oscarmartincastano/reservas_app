2731212351d62637e7043cee7c88e10fa22997ba
Arizaser <36188739+Arizaser@users.noreply.github.com>
2022-06-22 09:31:57 +0200

    dirección en editar usuario

--- resources/views/instalacion/users/edit.blade.php
+++ resources/views/instalacion/users/edit.blade.php
@@ -27,6 +27,10 @@
                                 <input value="{{ $user->email }}" name="email" type="email" placeholder="Email..." class="form-control col-md-10" required>
                             </div>
                             <div class="form-group row">
+                                <label class="col-md-2 control-label">Dirección</label>
+                                <input value="{{ $user->direccion }}" name="direccion" type="text" placeholder="Dirección..." class="form-control col-md-10" required>
+                            </div>
+                            <div class="form-group row">
                                 <label class="col-md-2 control-label">Cuota (opcional)</label>
                                 <input value="{{ $user->cuota }}" name="cuota" type="text" placeholder="Cuota..." class="form-control col-md-10">
                             </div>
