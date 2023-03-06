43b3b99e2eb3a232e49a3d2bfc8d7b91ecb4e353
rafacruz-te20 <rafacruz@tallerempresarial.es>
2023-03-06 11:34:38 +0100

    redirect login

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $exception)
+{
+    if ($exception instanceof TokenMismatchException) {
+        return redirect('/login');
+    }
+
+    return parent::render($request, $exception);
+}
 }
